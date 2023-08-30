514235b5192a33340e5c0919355df611f324c9a2
rmle23 <630645-rmle23@users.noreply.replit.com>
2023-08-30 21:10:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Area Calculator
+#
+import math
+
+# Triangle 
+base = 20
+height = 30
+area = base * height / 2
+print("The triangle area is ", area)
+
+# Rectangle
+length = 2
+width = 12
+area = length * width
+print("The rectangle area is ", area)
+
+# Circle
+radius = 36
+area = math.pi * radius * radius
+print("The circle area is ", area)
